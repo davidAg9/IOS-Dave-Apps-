434e6ee4c4759cc58ebd33cefc5db718701e2fe2
davidAg9 <shane_alpha9@outlook.com>
2021-08-15 17:28:55 +0000

    Initial Commit

---
+++ NetWorth/NetWorthApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetWorthApp.swift
+//  NetWorth
+//
+//  Created by Dave Agyakwa on 15/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct NetWorthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
